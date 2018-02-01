f337479ca1f777116ce2cfd68cec850ca47c8efb
DmitryZ <privetdime@ya.ru>
2018-02-01 18:52:19 +0300

    New Helper Class 1

---
+++ .ОбщийКод/СлучайныеЧисла.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace НашиПомощники
+{
+    public class СлучайныеЧисла
+    {
+        private static Random Генератор = new Random();
+
+        public static int Следующее (int Минимум, int Максимум)
+        {
+            return Генератор.Next(Минимум, Максимум + 1);
+        }
+    }
+}
--- Отправить.bat
+++ Отправить.bat
@@ -3,8 +3,7 @@ chcp 1251
 
 set /p MyMsg1="Enter Message: "
 
-git pull
-
 git add -A
 git commit -m "%MyMsg1%"
+git pull
 git push
